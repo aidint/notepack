9fefd33cd65d84623fd0542f34f0bda5c21b08ec
aidint <3762928+aidint@users.noreply.github.com>
2024-05-17 00:22:05 +0200

    integrate with tiptap

--- apps/frontend/src/components/note/Note.tsx
+++ apps/frontend/src/components/note/Note.tsx
@@ -1,9 +1,14 @@
-export function Note(){
+import { EditorProvider } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+
+const extensions = [StarterKit];
+
+export function Note() {
   return (
-    <div contentEditable={true} className="w-[var(--note-size-x)] h-[var(--note-size-y)] mx-[10px] my-[10px] border border-black border-dashed inline-block">
-    Note
+    <div className="w-[var(--note-size-x)] h-[var(--note-size-y)] mx-[10px] my-[10px] border border-black border-dashed inline-block">
+      <EditorProvider extensions={extensions} content={'Note'}></EditorProvider>
     </div>
-  )
+  );
 }
 
 export default Note;
