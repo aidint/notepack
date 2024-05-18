e559379d00636795cd4b1b1d3b26c4075b4960f6
aidint <3762928+aidint@users.noreply.github.com>
2024-05-18 23:49:59 +0200

    move to flexbox

--- apps/frontend/src/components/note/VerticalStack.tsx
+++ apps/frontend/src/components/note/VerticalStack.tsx
@@ -4,10 +4,10 @@ import { HorizontalStack } from './HorizontalStack';
 export function VerticalStack() {
   const [noteNumber, setNoteNumber] = useState(0)
   return (
-    <div>
+    <div className='flex flex-col'>
     {[...Array(noteNumber).keys()].map(number => <HorizontalStack rowNumber={number} key={`HS-${number}`}/>)
     }
-      <button className="bg-yellow-100 hover:bg-yellow-200 font-semibold py-2 px-4 rounded shadow block" onClick={() => setNoteNumber(noteNumber + 1)}>
+      <button className="bg-yellow-100 hover:bg-yellow-200 font-semibold py-2 px-4 rounded shadow block w-[100px]" onClick={() => setNoteNumber(noteNumber + 1)}>
         Click Me!
       </button>
     </div>
